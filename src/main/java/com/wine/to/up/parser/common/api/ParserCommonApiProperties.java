3afe039c7f51974730a2c5129a5ce8293bf2b753
Maxim <MaksKus1999@yandex.ru>
2020-10-05 16:35:33 +0300

    Refactor properties

--- src/main/java/com/wine/to/up/parser/common/api/ParserCommonApiProperties.java
+++ src/main/java/com/wine/to/up/parser/common/api/ParserCommonApiProperties.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotEmpty;
 /**
  * <p>
  *     Defines values from properties. Can be autowired as a regular class
@@ -15,15 +15,12 @@ import org.springframework.stereotype.Component;
  * @author Maxim Kuznetsov
  * @since 04.10.2020
  */
-@PropertySources({
-        @PropertySource("classpath:application-common-parser-api.properties"),
-        @PropertySource("classpath:application-common-parser-api-${spring.profiles.active:local}.properties")
-})
-@ConfigurationProperties(prefix = "common.parser.api")
-@Setter
 @Component
+@PropertySource("classpath:application-common-parser-api.properties")
+@ConfigurationProperties("parser.common.api")
+@Setter
 @Getter
 public class ParserCommonApiProperties {
-    private String messageProductTopicName;
-    private String host;
+    @NotEmpty
+    private String topicName;
 }
